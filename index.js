01ce02bfc174ed81ab077a7849f8dceabd126f6b
Ricky <ahujashaurya13@gmail.com>
2023-11-14 05:23:10 -0500

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // app config
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 5000;
@@ -7,6 +8,8 @@ mongoDB();
 
 // middleware
 app.use(express.json());
+app.use(cors());
+// might have to add fews lines from video if this doesnt work
 app.use("/api", require("./routes/CreateUser"));
 
 // api endpoints - making separate files for endpoints
