fbe81dff46bd1c97ab6e8ddd212470ca0a18176c
Ricky <ahujashaurya13@gmail.com>
2023-11-15 01:08:28 -0500

    added route for login page with validation

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // app config
-const cors = require("cors");
+// const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 5000;
 const mongoDB = require("./db");
 mongoDB();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 // might have to add fews lines from video if this doesnt work
 app.use("/api", require("./routes/CreateUser"));
 
--- routes/CreateUser.js
+++ routes/CreateUser.js
@@ -5,10 +5,9 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
 router.post(
-  "/createuser",
+  "/loginuser",
   [
     body("email").isEmail(),
-    body("name").isLength({ min: 5 }),
     body("password", "Invalid Password").isLength({ min: 5 }),
   ],
   async (req, res) => {
@@ -16,30 +15,58 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const { email, password } = req.body;
+    await User.findOne({ email })
+      .then((userData) => {
+        // if no userdata for that email if found then throw error
+        if (!userData) {
+          return res.status(400).send({ error: "Enter a valid Email" });
+        }
+
+        if (password !== userData.password) {
+          return res.status(400).send({ error: "Incorrect Password" });
+        }
+
+        return res.status(200).send(userData);
+      })
+      .catch((error) => res.status(400).send(error));
+  }
+);
+
+router.post(
+  "/createuser",
+  [
+    body("email").isEmail(),
+    body("name").isLength({ min: 5 }),
+    body("password", "Invalid Password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    // console.log(
+    //   req.body.name,
+    //   req.body.password,
+    //   req.body.email,
+    //   req.body.location
+    // );
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     // the above lines check for validation of various params n if there is no error then it goes ahead and creates the user
     // using the lines below
-    async (req, res) => {
-      try {
-        await User.create({
-          name: req.body.name,
-          password: req.body.password,
-          email: req.body.email,
-          location: req.body.location,
-        });
-        res.json({ success: true });
-      } catch (error) {
-        console.log(error);
-        res.json({ success: false });
-      }
-    };
+    try {
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+      // console.log(res);
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
   }
 );
 
 module.exports = router;
-
-// {
-//     name: "Ricky",
-//     password: "12345",
-//     email: "abcde@gmail.com",
-//     location: "chandigarh",
-//   }
