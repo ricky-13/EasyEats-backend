c2c06905ab1bafa8c0786d74f48d28d39bf1737f
Ricky <ahujashaurya13@gmail.com>
2023-11-16 05:10:50 -0500

    added jwt authentication using bcryptjs

--- db.js
+++ db.js
@@ -1,9 +1,80 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb+srv://ahujashaurya13:3vrsjxxgpM1lXrHt@cluster0.wg9ivqi.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://ahujashaurya13:3vrsjxxgpM1lXrHt@cluster0.wg9ivqi.mongodb.net/easyEats-mern?retryWrites=true&w=majority";
 
-const mongoDB = () => {
-  mongoose.connect(mongoURI);
+const mongoDB = async () => {
+  await mongoose.connect(mongoURI, { useNewUrlParser: true });
+  console.log("Connected to MongoDB");
+
+  const fetched_data = await mongoose.connection.db.collection("food_items");
+  const data = await fetched_data.find({}).toArray();
+
+  global.food_items = data;
+  console.log(global.food_items);
 };
 
 module.exports = mongoDB;
+
+// try {
+//   await mongoose.connect(mongoURI);
+//   console.log("Connected to MongoDB");
+
+//   const dbo = mongoose.connection.db;
+//   const dbi = dbo.db("easyeats-mern");
+
+//   const fetched_data = dbi.collection("food_items");
+//   const fdata = await fetched_data.find({});
+//   const adata = await fdata.toArray();
+
+//   console.log(adata);
+
+//   // global.food_items = data;
+//   // console.log(global.food_items);
+// } catch (err) {
+//   console.error("Error connecting to MongoDB:", err);
+// }
+
+module.exports = mongoDB;
+// const mongoDB = async () => {
+//   await mongoose.connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+//   console.log("connected to db");
+
+//   const db1 = mongoose.connection.db;
+
+//   // const dbo = db1.db("easyEats-mern");
+//   const data = await db1.collection("food_items").find({}).toArray();
+
+//   console.log(data);
+
+// const fetched_data = mongoose.connection.db.collection(
+//   "easyEats-mern.food_items"
+// );
+// console.log(fetched_data);
+//   const data = await mongoose.connection.db
+//     .collection("food_items")
+//     .find({})
+//     .toArray();
+// console.log(data);
+//   global.food_items = data;
+//   console.log(global.food_items);
+// } catch (err) {
+//   console.error("Error connecting to db : ", err);
+// }
+
+// const mongoDB = async () => {
+//   try {
+//     await mongoose.connect(mongoURI);
+//     console.log("Connected to MongoDB");
+
+//     const fetched_data = await mongoose.connection.db.collection("food_items");
+//     const data = await fetched_data.find({}).toArray();
+
+//     global.food_items = data;
+//     console.log(global.food_items);
+//   } catch (err) {
+//     console.error("Error connecting to MongoDB:", err);
+//   }
+// };
--- routes/CreateUser.js
+++ routes/CreateUser.js
@@ -3,6 +3,9 @@ const router = express.Router();
 // try straight with app.post
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const jwtSecret = "IWillGetAJobBeforeTheEndOf2023";
 
 router.post(
   "/loginuser",
@@ -23,11 +26,20 @@ router.post(
           return res.status(400).send({ error: "Enter a valid Email" });
         }
 
-        if (password !== userData.password) {
+        const passwordCompare = bcrypt.compare(password, userData.password);
+        if (!passwordCompare) {
           return res.status(400).send({ error: "Incorrect Password" });
         }
 
-        return res.status(200).send(userData);
+        const data = {
+          user: {
+            id: userData.id,
+          },
+        };
+
+        const Token = jwt.sign(data, jwtSecret);
+        // only body is sent in send function, put 2 values in curly braces and youre good as it goes as json
+        return res.status(200).send({ userData, Token });
       })
       .catch((error) => res.status(400).send(error));
   }
@@ -53,10 +65,15 @@ router.post(
     }
     // the above lines check for validation of various params n if there is no error then it goes ahead and creates the user
     // using the lines below
+
+    // hashing the password first using bcrypt for enhanced security
+    const salt = await bcrypt.genSalt(13);
+    let securePassword = await bcrypt.hash(req.body.password, salt);
+
     try {
       await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: securePassword,
         email: req.body.email,
         location: req.body.location,
       });
